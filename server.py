ce880c3fbbd4d55b412c7395da7d45b3257beddf
Tommy Wu <tommywu6@hotmail.com>
2020-05-20 16:37:18 +0800

    fix mqtt

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.config["DEBUG"] = True
 
 
 @app.route('/')
@@ -10,3 +9,4 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+
